41a9d5160d7ca7012950f43be3a951dcc06ba59c
Robert Schultz <rschultz@ancestry.com>
2013-05-26 22:19:54 -0700

    Add xunit project.

---
+++ SquarePeg.Service.Tests.Unit/BoardsServiceFacts.cs
@@ -0,0 +1,22 @@
+﻿using SquarePeg.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SquarePeg.Service.Tests.Unit
+{
+    public class BoardsServiceFacts
+    {
+        public class Ctor
+        {
+            [Fact]
+            public void NullBoardsRepositoryThrowsArgumentNullExceptoin()
+            {
+                Assert.Throws<ArgumentNullException>(() => new BoardsService(null, null));
+            }
+        }
+    }
+}
