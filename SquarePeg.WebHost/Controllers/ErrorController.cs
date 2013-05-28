1750f7316b36943b5c92442f2884486232722db7
Robert Schultz <rschultz@ancestry.com>
2013-05-27 23:30:28 -0700

    Sync.

---
+++ SquarePeg.WebHost/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SquarePeg.WebHost.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Http404()
+        {
+            return View();
+        }
+
+        public ActionResult Http500()
+        {
+            return this.View();
+        }
+
+    }
+}
--- SquarePeg.WebHost/Global.asax.cs
+++ SquarePeg.WebHost/Global.asax.cs
@@ -1,5 +1,8 @@
 ﻿namespace SquarePeg.WebHost
 {
+    using System;
+    using System.Net;
+    using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
@@ -15,6 +18,30 @@
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            
+
+            var error = Server.GetLastError();
+            var code = (error is HttpException) ? (error as HttpException).GetHttpCode() : 500;
+
+            this.Response.Clear();
+            this.Server.ClearError();
+
+            this.Response.TrySkipIisCustomErrors = true; 
+
+            if (code != 404)
+            {
+                this.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                this.Context.Server.TransferRequest("/Error/Http500");
+            }
+            else
+            {
+                this.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                this.Context.Server.TransferRequest("~/Error/Http404", true);
+            }
         }
     }
 }
